6c8341bab3828c5c60410c1e807edc872009da15
Shota999 <45195110+Shota999@users.noreply.github.com>
2022-11-22 12:01:07 +0400

    main

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,29 @@
+import React, { useState, useEffect } from "react";
 import './App.scss';
+import Header from "./Header";
+import AddContact from "./AddContact";
+import ContactList from "./ContactList";
 
 function App() {
+
+  const LOCAL_STORAGE_KEY = "contacts";
+  const [contacts, setContacts] = useState([]);
+
+  const addContactHandler = (contact) => {
+    setContacts([...contacts, contact]);
+  }
+  useEffect(() => {
+    const getContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (getContacts) setContacts(getContacts);
+  }, []);
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+  }, [contacts]);
   return (
-    <div className="App">
-      asdsad
+    <div className="ui container">
+      <Header />
+      <AddContact addContactHandler={addContactHandler} />
+      <ContactList contacts={contacts} />
     </div>
   );
 }
---
+++ src/components/ContactList.js
@@ -0,0 +1,12 @@
+import ContactCard from "./ContactCard";
+
+const ContactList = (props) => {
+  const renderContactList = props.contacts.map((contact) => {
+    return (
+        <ContactCard contact={contact} />
+    );
+  });
+  return <div className="ui celled list">{renderContactList}</div>;
+};
+
+export default ContactList;
